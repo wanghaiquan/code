098e531f83f75551a4054c73dba17166c17f50f2
cupdir <cupdir@gmail.com>
2019-07-14 19:15:58 +0800

    lcd

--- lcd.py
+++ lcd.py
@@ -1,57 +1,36 @@
-#!/usr/bin/python
-
-from lcd1602 import *
-from datetime import *
-import commands
+#!/usr/bin/env python
+import LCD1602
+import time
 
 
-def get_cpu_temp():
-    tmp = open('/sys/class/thermal/thermal_zone0/temp')
-    cpu = tmp.read()
-    tmp.close()
-    return '{:.2f}'.format(float(cpu) / 1000) + ' C'
+def setup():
+    LCD1602.init(0x27, 1)  # init(slave address, background light)
+    LCD1602.write(0, 0, '    Eeenjoy     ')
+    LCD1602.write(1, 1, 'Hello, World!')
+    time.sleep(2)
 
 
-def get_gpu_temp():
-    tmp = commands.getoutput(
-        'vcgencmd measure_temp|awk -F= \'{print $2}\'').replace('\'C', '')
-    gpu = float(tmp)
-    return '{:.2f}'.format(gpu) + ' C'
+def loop():
+    space = '                '
+    greetings = 'Thank you for buying SunFounder Sensor Kit for Raspberry! ^_^'
+    greetings = space + greetings
+    while True:
+        tmp = greetings
+        for i in range(0, len(greetings)):
+            LCD1602.write(0, 0, tmp)
+            tmp = tmp[1:]
+            time.sleep(0.8)
+            LCD1602.clear()
 
 
-def get_time_now():
-    return datetime.now().strftime('    %H:%M:%S\n   %Y-%m-%d')
+def destroy():
+    pass
 
 
-def get_ip_info():
-    return commands.getoutput('ifconfig wlan0|grep inet|awk -Faddr: \'{print $2}\'|awk \'{print $1}\'')
-
-
-def get_mem_info():
-    total = commands.getoutput('free -m|grep Mem:|awk \'{print $2}\'')
-    free = commands.getoutput('free -m|grep cache:|awk \'{print $4}\'')
-    return 'MEM:\n    ' + free + ' / ' + total + ' M'
-
-
-lcd = lcd1602(0x27, 1)
-lcd.clear()
-
-if __name__ == '__main__':
-
-    while(1):
-        lcd.clear()
-        lcd.message(get_ip_info())
-        sleep(5)
-
-        lcd.clear()
-        lcd.message(get_time_now())
-        sleep(5)
-
-        lcd.clear()
-        lcd.message(get_mem_info())
-        sleep(5)
-
-        lcd.clear()
-        lcd.message('CPU: ' + get_cpu_temp() + '\n')
-        lcd.message('GPU: ' + get_gpu_temp())
-        sleep(5)
+if __name__ == "__main__":
+    try:
+        setup()
+        while True:
+            pass
+    except KeyboardInterrupt:
+        destroy()
--- lcd1602.py
+++ lcd1602.py
@@ -1,246 +1,109 @@
-#!/usr/bin/python
-
-#
-# based on code from lrvick and LiquidCrystal
-# lrvic - https://github.com/lrvick/raspi-hd44780/blob/master/hd44780.py
-# LiquidCrystal - https://github.com/arduino/Arduino/blob/master/libraries/LiquidCrystal/LiquidCrystal.cpp
-#
-
-from time import sleep
-
-
-class lcd1602:
-
-    # commands
-    LCD_CLEARDISPLAY = 0x01
-    LCD_RETURNHOME = 0x02
-    LCD_ENTRYMODESET = 0x04
-    LCD_DISPLAYCONTROL = 0x08
-    LCD_CURSORSHIFT = 0x10
-    LCD_FUNCTIONSET = 0x20
-    LCD_SETCGRAMADDR = 0x40
-    LCD_SETDDRAMADDR = 0x80
-
-    # flags for display entry mode
-    LCD_ENTRYRIGHT = 0x00
-    LCD_ENTRYLEFT = 0x02
-    LCD_ENTRYSHIFTINCREMENT = 0x01
-    LCD_ENTRYSHIFTDECREMENT = 0x00
-
-    # flags for display on/off control
-    LCD_DISPLAYON = 0x04
-    LCD_DISPLAYOFF = 0x00
-    LCD_CURSORON = 0x02
-    LCD_CURSOROFF = 0x00
-    LCD_BLINKON = 0x01
-    LCD_BLINKOFF = 0x00
-
-    # flags for display/cursor shift
-    LCD_DISPLAYMOVE = 0x08
-    LCD_CURSORMOVE = 0x00
-
-    # flags for display/cursor shift
-    LCD_DISPLAYMOVE = 0x08
-    LCD_CURSORMOVE = 0x00
-    LCD_MOVERIGHT = 0x04
-    LCD_MOVELEFT = 0x00
-
-    # flags for function set
-    LCD_8BITMODE = 0x10
-    LCD_4BITMODE = 0x00
-    LCD_2LINE = 0x08
-    LCD_1LINE = 0x00
-    LCD_5x10DOTS = 0x04
-    LCD_5x8DOTS = 0x00
-
-    def __init__(self, pin_rs=14, pin_e=15, pins_db=[17, 18, 27, 22], GPIO=None):
-        # Emulate the old behavior of using RPi.GPIO if we haven't been given
-        # an explicit GPIO interface to use
-        if not GPIO:
-            import RPi.GPIO as GPIO
-        self.GPIO = GPIO
-        self.pin_rs = pin_rs
-        self.pin_e = pin_e
-        self.pins_db = pins_db
-
-        self.GPIO.setmode(GPIO.BCM)
-        self.GPIO.setwarnings(False)
-        self.GPIO.setup(self.pin_e, GPIO.OUT)
-        self.GPIO.setup(self.pin_rs, GPIO.OUT)
-
-        for pin in self.pins_db:
-            self.GPIO.setup(pin, GPIO.OUT)
-
-        self.write4bits(0x33)  # initialization
-        self.write4bits(0x32)  # initialization
-        self.write4bits(0x28)  # 2 line 5x7 matrix
-        self.write4bits(0x0C)  # turn cursor off 0x0E to enable cursor
-        self.write4bits(0x06)  # shift cursor right
-
-        self.displaycontrol = self.LCD_DISPLAYON | self.LCD_CURSOROFF | self.LCD_BLINKOFF
-
-        self.displayfunction = self.LCD_4BITMODE | self.LCD_1LINE | self.LCD_5x8DOTS
-        self.displayfunction |= self.LCD_2LINE
-
-        """ Initialize to default text direction (for romance languages) """
-        self.displaymode = self.LCD_ENTRYLEFT | self.LCD_ENTRYSHIFTDECREMENT
-        self.write4bits(self.LCD_ENTRYMODESET |
-                        self.displaymode)  # set the entry mode
-
-        self.clear()
-
-    def begin(self, cols, lines):
-
-        if (lines > 1):
-            self.numlines = lines
-            self.displayfunction |= self.LCD_2LINE
-            self.currline = 0
-
-    def home(self):
-
-        self.write4bits(self.LCD_RETURNHOME)  # set cursor position to zero
-        self.delayMicroseconds(3000)  # this command takes a long time!
-
-    def clear(self):
-
-        self.write4bits(self.LCD_CLEARDISPLAY)  # command to clear display
-        # 3000 microsecond sleep, clearing the display takes a long time
-        self.delayMicroseconds(3000)
-
-    def setCursor(self, col, row):
-
-        self.row_offsets = [0x00, 0x40, 0x14, 0x54]
-
-        if (row > self.numlines):
-            row = self.numlines - 1  # we count rows starting w/0
-
-        self.write4bits(self.LCD_SETDDRAMADDR | (col + self.row_offsets[row]))
-
-    def noDisplay(self):
-        """ Turn the display off (quickly) """
-
-        self.displaycontrol &= ~self.LCD_DISPLAYON
-        self.write4bits(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-
-    def display(self):
-        """ Turn the display on (quickly) """
-
-        self.displaycontrol |= self.LCD_DISPLAYON
-        self.write4bits(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-
-    def noCursor(self):
-        """ Turns the underline cursor on/off """
-
-        self.displaycontrol &= ~self.LCD_CURSORON
-        self.write4bits(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-
-    def cursor(self):
-        """ Cursor On """
-
-        self.displaycontrol |= self.LCD_CURSORON
-        self.write4bits(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-
-    def noBlink(self):
-        """ Turn on and off the blinking cursor """
-
-        self.displaycontrol &= ~self.LCD_BLINKON
-        self.write4bits(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-
-    def noBlink(self):
-        """ Turn on and off the blinking cursor """
-
-        self.displaycontrol &= ~self.LCD_BLINKON
-        self.write4bits(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-
-    def DisplayLeft(self):
-        """ These commands scroll the display without changing the RAM """
-
-        self.write4bits(self.LCD_CURSORSHIFT |
-                        self.LCD_DISPLAYMOVE | self.LCD_MOVELEFT)
-
-    def scrollDisplayRight(self):
-        """ These commands scroll the display without changing the RAM """
-
-        self.write4bits(self.LCD_CURSORSHIFT |
-                        self.LCD_DISPLAYMOVE | self.LCD_MOVERIGHT)
-
-    def leftToRight(self):
-        """ This is for text that flows Left to Right """
-
-        self.displaymode |= self.LCD_ENTRYLEFT
-        self.write4bits(self.LCD_ENTRYMODESET | self.displaymode)
-
-    def rightToLeft(self):
-        """ This is for text that flows Right to Left """
-        self.displaymode &= ~self.LCD_ENTRYLEFT
-        self.write4bits(self.LCD_ENTRYMODESET | self.displaymode)
-
-    def autoscroll(self):
-        """ This will 'right justify' text from the cursor """
-
-        self.displaymode |= self.LCD_ENTRYSHIFTINCREMENT
-        self.write4bits(self.LCD_ENTRYMODESET | self.displaymode)
-
-    def noAutoscroll(self):
-        """ This will 'left justify' text from the cursor """
-
-        self.displaymode &= ~self.LCD_ENTRYSHIFTINCREMENT
-        self.write4bits(self.LCD_ENTRYMODESET | self.displaymode)
-
-    def write4bits(self, bits, char_mode=False):
-        """ Send command to LCD """
-
-        self.delayMicroseconds(1000)  # 1000 microsecond sleep
-
-        bits = bin(bits)[2:].zfill(8)
-
-        self.GPIO.output(self.pin_rs, char_mode)
-
-        for pin in self.pins_db:
-            self.GPIO.output(pin, False)
-
-        for i in range(4):
-            if bits[i] == "1":
-                self.GPIO.output(self.pins_db[::-1][i], True)
-
-        self.pulseEnable()
-
-        for pin in self.pins_db:
-            self.GPIO.output(pin, False)
-
-        for i in range(4, 8):
-            if bits[i] == "1":
-                self.GPIO.output(self.pins_db[::-1][i - 4], True)
-
-        self.pulseEnable()
-
-    def delayMicroseconds(self, microseconds):
-        # divide microseconds by 1 million for seconds
-        seconds = microseconds / float(1000000)
-        sleep(seconds)
-
-    def pulseEnable(self):
-        self.GPIO.output(self.pin_e, False)
-        # 1 microsecond pause - enable pulse must be > 450ns
-        self.delayMicroseconds(1)
-        self.GPIO.output(self.pin_e, True)
-        # 1 microsecond pause - enable pulse must be > 450ns
-        self.delayMicroseconds(1)
-        self.GPIO.output(self.pin_e, False)
-        self.delayMicroseconds(1)		# commands need > 37us to settle
-
-    def message(self, text):
-        """ Send string to LCD. Newline wraps to second line"""
-
-        for char in text:
-            if char == '\n':
-                self.write4bits(0xC0)  # next line
-            else:
-                self.write4bits(ord(char), True)
+#!/usr/bin/env python
+
+import time
+import smbus
+
+BUS = smbus.SMBus(1)
+
+
+def write_word(addr, data):
+    global BLEN
+    temp = data
+    if BLEN == 1:
+        temp |= 0x08
+    else:
+        temp &= 0xF7
+    BUS.write_byte(addr, temp)
+
+
+def send_command(comm):
+    # Send bit7-4 firstly
+    buf = comm & 0xF0
+    buf |= 0x04               # RS = 0, RW = 0, EN = 1
+    write_word(LCD_ADDR, buf)
+    time.sleep(0.002)
+    buf &= 0xFB               # Make EN = 0
+    write_word(LCD_ADDR, buf)
+
+    # Send bit3-0 secondly
+    buf = (comm & 0x0F) << 4
+    buf |= 0x04               # RS = 0, RW = 0, EN = 1
+    write_word(LCD_ADDR, buf)
+    time.sleep(0.002)
+    buf &= 0xFB               # Make EN = 0
+    write_word(LCD_ADDR, buf)
+
+
+def send_data(data):
+    # Send bit7-4 firstly
+    buf = data & 0xF0
+    buf |= 0x05               # RS = 1, RW = 0, EN = 1
+    write_word(LCD_ADDR, buf)
+    time.sleep(0.002)
+    buf &= 0xFB               # Make EN = 0
+    write_word(LCD_ADDR, buf)
+
+    # Send bit3-0 secondly
+    buf = (data & 0x0F) << 4
+    buf |= 0x05               # RS = 1, RW = 0, EN = 1
+    write_word(LCD_ADDR, buf)
+    time.sleep(0.002)
+    buf &= 0xFB               # Make EN = 0
+    write_word(LCD_ADDR, buf)
+
+
+def init(addr, bl):
+    #	global BUS
+    #	BUS = smbus.SMBus(1)
+    global LCD_ADDR
+    global BLEN
+    LCD_ADDR = addr
+    BLEN = bl
+    try:
+        send_command(0x33)  # Must initialize to 8-line mode at first
+        time.sleep(0.005)
+        send_command(0x32)  # Then initialize to 4-line mode
+        time.sleep(0.005)
+        send_command(0x28)  # 2 Lines & 5*7 dots
+        time.sleep(0.005)
+        send_command(0x0C)  # Enable display without cursor
+        time.sleep(0.005)
+        send_command(0x01)  # Clear Screen
+        BUS.write_byte(LCD_ADDR, 0x08)
+    except:
+        return False
+    else:
+        return True
+
+
+def clear():
+    send_command(0x01)  # Clear Screen
+
+
+def openlight():  # Enable the backlight
+    BUS.write_byte(0x27, 0x08)
+    BUS.close()
+
+
+def write(x, y, str):
+    if x < 0:
+        x = 0
+    if x > 15:
+        x = 15
+    if y < 0:
+        y = 0
+    if y > 1:
+        y = 1
+
+    # Move cursor
+    addr = 0x80 + 0x40 * y + x
+    send_command(addr)
+
+    for chr in str:
+        send_data(ord(chr))
 
 
 if __name__ == '__main__':
-
-    lcd = lcd1602()
-    lcd.clear()
-    lcd.message("hello world!")
+    init(0x27, 1)
+    write(4, 0, 'Hello')
+    write(7, 1, 'world!')
