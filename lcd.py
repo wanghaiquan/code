969aa1f03bff610ae0ce900181c1ee172a1d2d4f
cupdir <cupdir@gmail.com>
2019-07-14 19:31:52 +0800

    dddd

--- lcd.py
+++ lcd.py
@@ -5,7 +5,7 @@ import time
 
 def setup():
     LCD1602.init(0x27, 1)  # init(slave address, background light)
-    LCD1602.write(0, 0, '    海泉     ')
+    LCD1602.write(0, 0, '    haiquan     ')
     LCD1602.write(1, 1, 'Hello, World!')
     time.sleep(2)
 
