5df4d6469e7fbd52d62f6fe47f8d8d8063d4822b
cupdir <cupdir@gmail.com>
2019-07-15 12:16:39 +0800

    ddd

--- led.py
+++ led.py
@@ -1,46 +1,38 @@
-#!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
 
-pins = {'pin_R': 12, 'pin_G': 11}  # pins is a dicts
-sleep_time = 0.5
+LED_R = 17
+LED_G = 18
 
-GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-for i in pins:
-    GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
-    GPIO.output(pins[i], GPIO.LOW)  # Set pins to low(0V) to off led
-    print("i is ", i, pins[i])
+GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(LED_R, GPIO.OUT)
+GPIO.setup(LED_G, GPIO.OUT)
 
-def loop():
-    while True:
-        # Set pins to high(+3.3V) to on led
-        GPIO.output(pins['pin_R'], GPIO.HIGH)
-        print(pins['pin_R'], " Red Led is On...")
-        time.sleep(sleep_time)
+pwm_r = GPIO.PWM(LED_R, 2000)
+pwm_g = GPIO.PWM(LED_G, 2000)
 
-        GPIO.output(pins['pin_R'], GPIO.LOW)
-        print(pins['pin_R'], " Red Led is off...")
-        time.sleep(sleep_time)
+GPIO.output(LED_R, GPIO.LOW)
+GPIO.output(LED_G, GPIO.LOW)
 
-        GPIO.output(pins['pin_G'], GPIO.HIGH)
-        print(pins['pin_G'], " Green Led is On...")
-        time.sleep(sleep_time)
-
-        GPIO.output(pins['pin_G'], GPIO.LOW)
-        print(pins['pin_G'], " Green Led is off...")
-        time.sleep(sleep_time)
-
-
-def destroy():
-
-    for i in pins:
-        GPIO.output(pins[i], GPIO.LOW)    # Turn off all leds
-    GPIO.cleanup()
+pwm_r.start(0)
+pwm_g.start(100)
 
 
 if __name__ == "__main__":
     try:
-        loop()
+        while True:
+            for i in range(100):
+                pwm_r.ChangeDutyCycle(i)
+                pwm_g.ChangeDutyCycle(100 - i)
+                time.sleep(.25)
+
+            for i in range(100):
+                pwm_r.ChangeDutyCycle(100 - i)
+                pwm_g.ChangeDutyCycle(i)
+                time.sleep(.25)
     except KeyboardInterrupt:
-        destroy()
+        pwm_r.stop()
+        pwm_g.stop()
+        GPIO.cleanup()
+        exit(6)
