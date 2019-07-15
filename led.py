043d506c3a56c3f2049699ac4c32b89ff69d6324
cupdir <cupdir@gmail.com>
2019-07-15 11:09:15 +0800

    led

---
+++ led.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+pins = {'pin_R': 11, 'pin_G': 12}  # pins is a dict
+sleep_time = 0.5
+
+GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+for i in pins:
+    GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
+    GPIO.output(pins[i], GPIO.LOW)  # Set pins to low(0V) to off led
+    print("i is ", i, pins[i])
+
+
+def loop():
+    while True:
+        # Set pins to high(+3.3V) to on led
+        GPIO.output(pins['pin_R'], GPIO.HIGH)
+        print(pins['pin_R'], " Red Led is On...")
+        time.sleep(sleep_time)
+
+        GPIO.output(pins['pin_R'], GPIO.LOW)
+        print(pins['pin_R'], " Red Led is off...")
+        time.sleep(sleep_time)
+
+        GPIO.output(pins['pin_G'], GPIO.HIGH)
+        print(pins['pin_G'], " Green Led is On...")
+        time.sleep(sleep_time)
+
+        GPIO.output(pins['pin_G'], GPIO.LOW)
+        print(pins['pin_G'], " Green Led is off...")
+        time.sleep(sleep_time)
+
+
+def destroy():
+
+    for i in pins:
+        GPIO.output(pins[i], GPIO.LOW)    # Turn off all leds
+    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
