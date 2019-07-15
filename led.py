d2517abc82a53e0de28f45da6d0090101db73c60
cupdir <cupdir@gmail.com>
2019-07-15 11:46:44 +0800

    ...

--- led.py
+++ led.py
@@ -2,45 +2,26 @@
 import RPi.GPIO as GPIO
 import time
 
-pins = {'pin_R': 11, 'pin_G': 12}  # pins is a dict
-sleep_time = 0.5
-
-GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-for i in pins:
-    GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
-    GPIO.output(pins[i], GPIO.LOW)  # Set pins to low(0V) to off led
-    print("i is ", i, pins[i])
+REDPIN = 11
+GREENPIN = 12
+pins = (REDPIN, GREENPIN)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pins, GPIO.OUT)
+GPIO.output(pins, GPIO.LOW)
 
 
-def loop():
-    while True:
-        # Set pins to high(+3.3V) to on led
-        GPIO.output(pins['pin_R'], GPIO.HIGH)
-        print(pins['pin_R'], " Red Led is On...")
-        time.sleep(sleep_time)
-
-        GPIO.output(pins['pin_R'], GPIO.LOW)
-        print(pins['pin_R'], " Red Led is off...")
-        time.sleep(sleep_time)
-
-        GPIO.output(pins['pin_G'], GPIO.HIGH)
-        print(pins['pin_G'], " Green Led is On...")
-        time.sleep(sleep_time)
-
-        GPIO.output(pins['pin_G'], GPIO.LOW)
-        print(pins['pin_G'], " Green Led is off...")
-        time.sleep(sleep_time)
+def Red(duration):
+    GPIO.output(REDPIN, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(REDPIN, GPIO.LOW)
 
 
-def destroy():
+def Green(duration):
+    GPIO.output(GREENPIN, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(GREENPIN, GPIO.LOW)
 
-    for i in pins:
-        GPIO.output(pins[i], GPIO.LOW)    # Turn off all leds
-    GPIO.cleanup()
 
-
-if __name__ == "__main__":
-    try:
-        loop()
-    except KeyboardInterrupt:
-        destroy()
+Red(5)
+Green(5)
+GPIO.cleanup()
