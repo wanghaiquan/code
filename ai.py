e68ef46d6858d8c3fbd2f77f6639d650e6ad12c0
cupdir <cupdir@gmail.com>
2019-08-02 01:30:08 +0800

    asd

--- ai.py
+++ ai.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-from lib.google_home_led_pattern import GoogleHomeLedPattern
-from lib.alexa_led_pattern import AlexaLedPattern
-from lib.pixels import Pixels, pixels
 from snowboy import snowboydecoder
-import sys
-import os
-# __file__获取执行文件相对路径，整行为取上一级的上一级目录
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
+from pixels import Pixels, pixels
+from alexa_led_pattern import AlexaLedPattern
+from google_home_led_pattern import GoogleHomeLedPattern
 
 
 def detected_callback():
