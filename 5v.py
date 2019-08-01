2df3706a4960bb5df8faf81b5e5e3b0deccac565
cupdir <cupdir@gmail.com>
2019-08-01 13:21:33 +0800

    asdasd

--- 5v.py
+++ 5v.py
@@ -10,10 +10,10 @@ num = 17  # 树莓派针脚编号
 GPIO.setup(num, GPIO.OUT)
 id = GPIO.input(num)
 
-if (id == 1):
-    GPIO.output(num, GPIO.LOW)
-    print "已打开设备"
-if (id == 0):
-    GPIO.output(num, GPIO.HIGH)
-    print "已关闭设备"
+GPIO.output(num, GPIO.LOW)
+
+time.sleep(10)
+
+GPIO.output(num, GPIO.HIGH)
+
 GPIO.cleanup()
