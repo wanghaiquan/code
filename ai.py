987664b0741859ff2768a5d15cd3ee756302d5ff
cupdir <cupdir@gmail.com>
2019-08-02 02:09:21 +0800

    asd

--- ai.py
+++ ai.py
@@ -14,18 +14,9 @@ def detected_callback():
     pixels.off()
 
 
-def interrupt_callback():
-    print "收到了...."
-
-
 if __name__ == '__main__':
     pixels.pattern = GoogleHomeLedPattern(show=pixels.show)
     detector = snowboydecoder.HotwordDetector(
         "yingzi.pmdl", sensitivity=0.5, audio_gain=1)
-    callbacks = [lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING),
-                 lambda: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)]
-    print('Listening... Press Ctrl+C to exit')
-    detector.start(detected_callback=callbacks,
-                   interrupt_check=interrupt_callback,
-                   sleep_time=0.03)
-    detector.terminate()
+
+    detector.start(detected_callback)
