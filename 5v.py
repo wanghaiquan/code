d0af7e9277c343a6fb0016818b97ef2a63afd149
cupdir <cupdir@gmail.com>
2019-08-01 23:14:36 +0800

    asd

--- 5v.py
+++ 5v.py
@@ -49,7 +49,7 @@ def main():
 
         GPIO.output(RelayPin, GPIO.LOW)
         GPIO.output(RelayPin, False)
-        time.sleep(10)
+        time.sleep(1)
 
         print ('|*****************|')
         print ('|  Relay open...  |')
@@ -58,7 +58,7 @@ def main():
         # connect
         GPIO.output(RelayPin, GPIO.HIGH)
         GPIO.output(RelayPin, True)
-        time.sleep(10)
+        time.sleep(1)
 
 # define a destroy function for clean up everything after the script finished
 
