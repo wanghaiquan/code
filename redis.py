c2bf1c7dec5c0a5d21402706027ffe4411f666a0
cupdir <cupdir@gmail.com>
2019-08-04 18:51:28 +0800

    asd

--- redis.py
+++ redis.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import redis
+from redis_queue import RedisQueue
+import time
 
 
-class Task(object):
-    def __init__(self):
-        self.rcon = redis.StrictRedis(host='localhost', db=5)
-        self.queue = 'task:prodcons:queue'
+class RedisQueue(object):
+    def __init__(self, name, namespace='queue', **redis_kwargs):
+        # redis的默认参数为：host='localhost', port=6379, db=0， 其中db为定义redis database的数量
+        self.__db = redis.Redis(**redis_kwargs)
+        self.key = '%s:%s' % (namespace, name)
 
-    def listen_task(self):
-        while True:
-            task = self.rcon.blpop(self.queue, 0)[1]
-            print "Task get", task
+    def qsize(self):
+        return self.__db.llen(self.key)  # 返回队列里面list内元素的数量
+
+    def put(self, item):
+        self.__db.rpush(self.key, item)  # 添加新元素到队列最右方
+
+    def get_wait(self, timeout=None):
+        # 返回队列第一个元素，如果为空则等待至有元素被加入队列（超时时间阈值为timeout，如果为None则一直等待）
+        item = self.__db.blpop(self.key, timeout=timeout)
+        # if item:
+        #     item = item[1]  # 返回值为一个tuple
+        return item
+
+    def get_nowait(self):
+        # 直接返回队列第一个元素，如果队列为空返回的是None
+        item = self.__db.lpop(self.key)
+        return item
 
 
-if __name__ == '__main__':
-    print 'listen task queue'
-    Task().listen_task()
+q = RedisQueue('rq')  # 新建队列名为rq
+for i in range(5):
+    q.put(i)
+    print "input.py: data {} enqueue {}".format(i, time.strftime("%c"))
+    time.sleep(1)
