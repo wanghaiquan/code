dccd3f65ce78963182dcf90440f24db808ada90b
cupdir <cupdir@gmail.com>
2019-08-05 18:13:47 +0800

    asdasd

--- 5v.py
+++ 5v.py
@@ -53,7 +53,7 @@ def main():
             # GPIO.output(RelayPin, GPIO.HIGH)
             print queue
             time.sleep(4)
-            GPIO.output(RelayPin, False)
+            GPIO.output(RelayPin, True)
             # print int(humidity)
             # time.sleep(5)
         # 温度大于90给电压
