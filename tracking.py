ad82c9b594deaae178c0f627f4d64005bfd17fc2
cupdir <cupdir@gmail.com>
2019-08-13 14:24:44 +0800

    asdasd

--- tracking.py
+++ tracking.py
@@ -183,8 +183,10 @@ try:
     init()
     key_scan()
     while True:
-        print("cm = %f" % distance())
+        # print("cm = %f" % distance())
         dis = distance()
+        print dis
+
         # 检测到黑线时循迹模块相应的指示灯亮，端口电平为LOW
         # 未检测到黑线时循迹模块相应的指示灯灭，端口电平为HIGH
         TrackSensorLeftValue1 = GPIO.input(TrackSensorLeftPin1)
