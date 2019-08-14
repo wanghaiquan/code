b23887ba9e6308d09ae5e60078fec3877f323587
cupdir <cupdir@gmail.com>
2019-08-14 17:00:39 +0800

    asd

--- epd2in9b.py
+++ epd2in9b.py
@@ -38,7 +38,7 @@ try:
     drawblack = ImageDraw.Draw(HBlackimage)
     drawry = ImageDraw.Draw(HRYimage)
     drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
-    drawblack.text((10, 20), '2.9inch e-Paper bc', font = font24, fill = 0)
+    drawblack.text((10, 20), '想法、行动、创造', font = font24, fill = 0)
     drawblack.text((150, 0), u'少儿创客工场', font = font24, fill = 0)
     drawblack.line((20, 50, 70, 100), fill = 0)
     drawblack.line((70, 50, 20, 100), fill = 0)
@@ -51,39 +51,7 @@ try:
     epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
     time.sleep(2)
 
-    # Drawing on the Vertical image
-    logging.info("2.Drawing on the Vertical image...")
-    LBlackimage = Image.new('1', (epd.width, epd.height), 255)  # 126*298
-    LRYimage = Image.new('1', (epd.width, epd.height), 255)  # 126*298
-    drawblack = ImageDraw.Draw(LBlackimage)
-    drawry = ImageDraw.Draw(LRYimage)
 
-    drawblack.text((2, 0), 'hello world', font = font18, fill = 0)
-    drawblack.text((2, 20), '2.9inch epd bc', font = font18, fill = 0)
-    drawblack.text((20, 50), u'少儿创客工场', font = font18, fill = 0)
-    drawblack.line((10, 90, 60, 140), fill = 0)
-    drawblack.line((60, 90, 10, 140), fill = 0)
-    drawblack.rectangle((10, 90, 60, 140), outline = 0)
-    drawry.line((95, 90, 95, 140), fill = 0)
-    drawry.line((70, 115, 120, 115), fill = 0)
-    drawry.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    drawry.rectangle((10, 150, 60, 200), fill = 0)
-    drawry.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(LBlackimage), epd.getbuffer(LRYimage))
-    time.sleep(2)
-
-    logging.info("3.read bmp file")
-    HBlackimage = Image.open(os.path.join(picdir, '2in9bc-b.bmp'))
-    HRYimage = Image.open(os.path.join(picdir, '2in9bc-ry.bmp'))
-    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
-    time.sleep(2)
-
-    logging.info("4.read bmp file on window")
-    blackimage1 = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-    redimage1 = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-    newimage = Image.open(os.path.join(picdir, '100x100.bmp'))
-    blackimage1.paste(newimage, (50,10))
-    epd.display(epd.getbuffer(blackimage1), epd.getbuffer(redimage1))
 
     logging.info("Clear...")
     epd.init()
