d028c568d1939bba8c194e322612269f99cc7a28
cupdir <cupdir@gmail.com>
2019-08-15 12:02:49 +0800

    asdasd

--- tracking.py
+++ tracking.py
@@ -185,7 +185,7 @@ try:
     while True:
         # print("cm = %f" % distance())
         dis = distance()
-        time.sleep(2)
+        time.sleep(0.5)
 
         # 检测到黑线时循迹模块相应的指示灯亮，端口电平为LOW
         # 未检测到黑线时循迹模块相应的指示灯灭，端口电平为HIGH
