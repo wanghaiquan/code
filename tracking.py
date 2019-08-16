b69e0090d74fc5a0aadf032eebcac8ac959cd580
cupdir <cupdir@gmail.com>
2019-08-16 15:24:39 +0800

    werwer

--- tracking.py
+++ tracking.py
@@ -85,7 +85,7 @@ def distance():
 
 
 def run(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
     GPIO.output(IN3, GPIO.HIGH)
     GPIO.output(IN4, GPIO.LOW)
