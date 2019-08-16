339ffa18814a5f4f1d81ceb29fb1e63e0b400ef7
cupdir <cupdir@gmail.com>
2019-08-16 11:35:19 +0800

    asdasd

--- tracking.py
+++ tracking.py
@@ -87,7 +87,7 @@ def distance():
 def run(leftspeed, rightspeed):
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.LOW)
     pwm_ENA.ChangeDutyCycle(leftspeed)
     pwm_ENB.ChangeDutyCycle(rightspeed)
