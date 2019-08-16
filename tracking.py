2ad518396e0fcb5b1fb0c3d1ab5168db782de3a0
cupdir <cupdir@gmail.com>
2019-08-16 16:06:17 +0800

    asdas

--- tracking.py
+++ tracking.py
@@ -86,8 +86,8 @@ def distance():
 
 def run(leftspeed, rightspeed):
     GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.LOW)
     pwm_ENA.ChangeDutyCycle(leftspeed)
     pwm_ENB.ChangeDutyCycle(rightspeed)
@@ -213,7 +213,7 @@ try:
         # 0 1 1 0
         elif TrackSensorLeftValue2 == True and TrackSensorRightValue1 == True:
             print 'run'
-            run(100, 100)
+            run(100, 0)
 
 except KeyboardInterrupt:
     pass
