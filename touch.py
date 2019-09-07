263d6ca96720319d71862b7a3fba1299193f4012
cupdir <cupdir@gmail.com>
2019-09-07 18:52:03 +0800

    asdasd

---
+++ touch.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+import os
+
+#sensor pin define
+touch_pin = 18
+touchstatus = False
+
+def init():
+         GPIO.setwarnings(False)
+         GPIO.setmode(GPIO.BCM)
+         GPIO.setup(touch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+         pass
+
+#read digital touch sensor
+def read_touchsensor():
+         global touchstatus
+         if (GPIO.input(touch)==True):
+                  touchstatus = not touchstatus
+                  if touchstatus:
+                           print"Turn on relay"
+         print"\n"
+                           buzzer_on()
+                           relay_on()
+
+                  else:
+                           print"Turn off relay"
+         print"\n"
+                           # buzzer_on()
+                           # relay_off()
+         pass
+
+
+#main loop
+def main():
+         print"...................................................................System initializing..."
+         init()
+         # buzzer_off()
+         # relay_off()
+         print"...................................................................Ok"
+         print"...................................................................Please touch"
+         print"\n"
+         while True:
+                  read_touchsensor()
+
+if __name__ == '__main__':
+         try:
+                  main()
+                  pass
+         except KeyboardInterrupt:
+                  pass
+         pass
+GPIO.cleanup()
