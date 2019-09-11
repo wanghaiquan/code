dbb6664f580bdd0c93de24dc9ca2bfbb37e239da
cupdir <cupdir@gmail.com>
2019-09-11 11:05:07 +0800

    asd

--- touch.py
+++ touch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
 import os
@@ -8,8 +9,40 @@ import os
 touch = 18
 buzzer = 24
 
+# mqtt function
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: "+str(rc))
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+mqttc = mqtt.Client(transport="websockets")
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+# Uncomment to enable debug messages
+mqttc.on_log = on_log
+
+
+mqttc.connect("192.144.190.105", 8083, 60)
+mqttc.subscribe("$SYS/mqtt/light", 0)
+mqttc.loop_forever()
+# end
 
 def init():
+
+
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(buzzer, GPIO.OUT)
