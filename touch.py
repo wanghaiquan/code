e6155e473fd7c8eba89abaf694623578967f2cf1
cupdir <cupdir@gmail.com>
2019-09-11 11:30:14 +0800

    asd

--- touch.py
+++ touch.py
@@ -10,38 +10,7 @@ import json
 touch = 18
 buzzer = 24
 
-# mqtt function
 
-def on_connect(mqttc, obj, flags, rc):
-    print("rc: "+str(rc))
-
-def on_message(mqttc, obj, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-    message = json.loads(msg.payload)
-    print(str(message))
-
-def on_publish(mqttc, obj, mid):
-    print("mid: "+str(mid))
-
-def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
-
-def on_log(mqttc, obj, level, string):
-    print(string)
-
-mqttc = mqtt.Client(transport="websockets")
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
-# Uncomment to enable debug messages
-mqttc.on_log = on_log
-
-
-mqttc.connect("192.144.190.105", 8083, 60)
-mqttc.subscribe("mqtt/light", 0)
-mqttc.loop_forever()
-# end
 
 def init():
 
@@ -82,9 +51,43 @@ def read_touchsensor():
 
     pass
 
+# mqtt function
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: "+str(rc))
+
+def on_message(mqttc, obj, msg):
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    message = json.loads(msg.payload)
+    print(str(message))
+
+def on_publish(mqttc, obj, mid):
+    print("mid: "+str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+# end
+
+
 
 # main loop
 def main():
+    mqttc = mqtt.Client(transport="websockets")
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    # Uncomment to enable debug messages
+    mqttc.on_log = on_log
+
+
+    mqttc.connect("192.144.190.105", 8083, 60)
+    mqttc.subscribe("mqtt/light", 0)
+    mqttc.loop_forever()
+
     print"...................................................................System initializing..."
     init()
     buzzer_off()
